257ee69e8c83aca509e059ff9b979b2169271c88
Emanuel Rosal <emanuel.rosa@naranja.com>
2020-03-05 17:35:57 -0300

    add example env

--- .vault-refresh-cache-list.sh
+++ .vault-refresh-cache-list.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+echo "Creating vault cache list \"${_vault_cache_file}\"..."
+
 curl -sk -H "X-Vault-Token: ${_vault_token}" \
-    --request LIST "${_vault_corp_url}" | jq -r >> $_vault_cache_file
+    --request LIST "${_vault_corp_url}" | jq -r > $_vault_cache_file
