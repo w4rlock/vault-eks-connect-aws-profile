ef4e2d7462f96b972197c834220d62988d964afa
Emanuel Rosal <emanuel.rosa@naranja.com>
2020-03-05 17:28:27 -0300

    fc

--- .vault-refresh-cache-list.sh
+++ .vault-refresh-cache-list.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-source .env
 
 curl -sk -H "X-Vault-Token: ${_vault_token}" \
-    --request LIST "${_vault_corp_url}" | tee $_vault_cache_file
+    --request LIST "${_vault_corp_url}" | jq -r >> $_vault_cache_file
